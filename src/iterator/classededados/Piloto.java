986a070c936d15877b70a95fa33aa7abf92371da
Pedro Queiroz <pqlb1512@gmail.com>
2025-04-04 15:11:45 -0300

    adição do iterator(incompleto)

---
+++ src/iterator/classededados/Piloto.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iterator.classededados;
+
+/**
+ * @author pedro
+ */
+public class Piloto {
+    //Atributos
+    private String codigo;
+    private String nome;
+    private String pais;
+    private String idade;
+    private String equipe;
+    private String fabricanteMotor;
+    private String pontos;
+    //Metodos
+
+
+    public Piloto(String codigo, String nome, String pais, String idade, String equipe, String fabricanteMotor, String pontos) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.pais = pais;
+        this.idade = idade;
+        this.equipe = equipe;
+        this.fabricanteMotor = fabricanteMotor;
+        this.pontos = pontos;
+    }
+
+    public Piloto(String str) {
+        String[] vetor = str.split(";");
+        this.codigo = vetor[0];
+        this.nome = vetor[1];
+        this.pais = vetor[2];
+        this.idade = vetor[3];
+        this.equipe = vetor[4];
+        this.fabricanteMotor = vetor[5];
+        this.pontos = vetor[6];
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(String equipe) {
+        this.equipe = equipe;
+    }
+
+    public String getFabricanteMotor() {
+        return fabricanteMotor;
+    }
+
+    public void setFabricanteMotor(String fabricanteMotor) {
+        this.fabricanteMotor = fabricanteMotor;
+    }
+
+    public String getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(String pontos) {
+        this.pontos = pontos;
+    }
+}
---
+++ src/iterator/persistencia/PilotoPersistencia.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iterator.persistencia;
+
+import iterator.classededados.Piloto;
+import iterator.classededados.ColecaoDeObjetos;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+/**
+ * @author pedro
+ */
+public class PilotoPersistencia {
+    //Atributos
+    private String nomeDoArquivo;
+
+    //Metodos
+    public PilotoPersistencia(String nome) {
+        nomeDoArquivo = nome;
+    }
+
+    private int quantidadeDeAlunosNoArquivo() throws Exception {
+        try {
+            int quantidade = 0;
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linha = "";
+            while ((linha = br.readLine()) != null) {
+                quantidade++;
+            }
+            br.close();
+            return quantidade;
+        } catch (Exception erro) {
+            throw erro;
+        }
+    }
+
+    public Iterator listagemDePilotosLinkedList() throws Exception {
+        try {
+            LinkedList<Piloto> lista = new LinkedList<>();
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linha = "";
+            while ((linha = br.readLine()) != null) {
+                Piloto aux = new Piloto(linha);  // Aqui assume-se que o Piloto tem um construtor que recebe uma String
+                lista.add(aux);  // Adiciona à LinkedList
+            }
+            br.close();
+            return lista.iterator();  // Retorna um Iterator da LinkedList
+        } catch (Exception erro) {
+            throw erro;
+        }
+    }
+
+    public Iterator listagemDePilotosTreeSet() throws Exception {
+        try {
+            TreeSet<Piloto> conjuntoOrdenado = new TreeSet<>(new Comparator<Piloto>() {
+                public int compare(Piloto p1, Piloto p2) {
+                    // Compara os pilotos pelo código, retornando em ordem alfabética
+                    return p1.getNome().compareTo(p2.getNome());
+                }
+            });
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linha = "";
+            while ((linha = br.readLine()) != null) {
+                Piloto aux = new Piloto(linha);
+                conjuntoOrdenado.add(aux);  // Adiciona ao TreeSet
+            }
+            br.close();
+            return conjuntoOrdenado.iterator();  // Retorna um Iterator do TreeSet
+        } catch (Exception erro) {
+            throw erro;
+        }
+    }
+
+    public Iterator listagemDePilotosHashSet() throws Exception {
+        try {
+            HashSet<Piloto> conjuntoUnico = new HashSet<>();
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linha = "";
+            while ((linha = br.readLine()) != null) {
+                Piloto aux = new Piloto(linha);
+                conjuntoUnico.add(aux);  // Adiciona ao HashSet, mantendo unicidade
+            }
+            br.close();
+            return conjuntoUnico.iterator();  // Retorna o Iterator do HashSet
+        } catch (Exception erro) {
+            throw erro;
+        }
+    }
+
+    public Iterator listagemDePilotosPriorityQueue() throws Exception {
+        try {
+            PriorityQueue<Piloto> filaPrioritaria = new PriorityQueue<>(new Comparator<Piloto>() {
+                public int compare(Piloto p1, Piloto p2) {
+                    return p1.getCodigo().compareTo(p2.getCodigo()); // Pilotos com mais pontos têm maior prioridade
+                }
+            });
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linha = "";
+            while ((linha = br.readLine()) != null) {
+                Piloto aux = new Piloto(linha);
+                filaPrioritaria.add(aux);  // Adiciona à PriorityQueue
+            }
+            br.close();
+            return filaPrioritaria.iterator();  // Retorna o Iterator da PriorityQueue
+        } catch (Exception erro) {
+            throw erro;
+        }
+    }
+
+    public Iterator listagemDePilotosQueue() throws Exception {
+        try {
+            Queue<Piloto> fila = new LinkedList<>();
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linha = "";
+            while ((linha = br.readLine()) != null) {
+                Piloto aux = new Piloto(linha);
+                fila.add(aux);  // Adiciona à fila (LinkedList implementando Queue)
+            }
+            br.close();
+            return fila.iterator();  // Retorna o Iterator da fila
+        } catch (Exception erro) {
+            throw erro;
+        }
+    }
+
+}
